cb7c55631914bfeb86c5e55daf612f6d914ba8ac
koiNoCirculation <belica.ly42@gmail.com>
2024-09-04 13:34:42 -0700

    apply spotless

--- src/main/java/appeng/me/cluster/implementations/TriFunction.java
+++ src/main/java/appeng/me/cluster/implementations/TriFunction.java
@@ -1,8 +1,6 @@
 package appeng.me.cluster.implementations;
 
 import java.io.Serializable;
-import java.util.Objects;
-import java.util.function.Function;
 
 @FunctionalInterface
 public interface TriFunction<A1, A2, A3> extends Serializable {
@@ -16,15 +14,4 @@ public interface TriFunction<A1, A2, A3> extends Serializable {
      * @return the function result
      */
     void apply(A1 a1, A2 a2, A3 a3);
-
-    /**
-     * Returns a composed function that first applies this function to its input, and then applies the {@code after}
-     * function to the result. If evaluation of either function throws an exception, it is relayed to the caller of the
-     * composed function.
-     *
-     * @param <V>   the type of output of the {@code after} function, and of the composed function
-     * @param after the function to apply after this function is applied
-     * @return a composed function that first applies this function and then applies the {@code after} function
-     * @throws NullPointerException if after is null
-     */
 }
